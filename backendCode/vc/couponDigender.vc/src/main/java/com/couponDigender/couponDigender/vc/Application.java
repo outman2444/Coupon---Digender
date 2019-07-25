545ec0374463bbca68baed6bb98241b337048749
outman <18635062444@163.com>
2019-07-25 13:50:03 +0800

    创建本地开发分支

--- backendCode/vc/couponDigender.vc/src/main/java/com/couponDigender/couponDigender/vc/Application.java
+++ backendCode/vc/couponDigender.vc/src/main/java/com/couponDigender/couponDigender/vc/Application.java
@@ -3,6 +3,9 @@ package com.couponDigender.couponDigender.vc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 2019-07-25
+ */
 @SpringBootApplication
 public class Application {
 
